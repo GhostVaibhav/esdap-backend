8bfe528d6a2048eebff2307a10ca68e883a54648
cosmos-127 <cosmos.dev.127@gmail.com>
2024-03-21 00:24:58 +0530

    file structuring and index file initialising

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// index.js
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
